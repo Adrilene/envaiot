46eba64913ab9f1cfcd35cebbb00c13bc869a854
Adrilene <adrilenefonseca@gmail.com>
2023-01-05 22:29:25 -0300

    reorganizing

--- Configurator/run.py
+++ Configurator/run.py
@@ -3,5 +3,5 @@ from project import app
 
 if __name__ == "__main__":
     port = 5000
-    print(f"Running Configurator (Middleware) port:{port} \n")
+    print(f"Running Configurator port:{port} \n")
     app.run(port=port, debug=True)
