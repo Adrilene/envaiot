128e67ecf36ca883143b0aa2d9b1dbd3759d1bf6
Adrilene <adrilenefonseca@gmail.com>
2023-01-09 16:01:00 -0300

    update to env var

--- Configurator/project/requests.py
+++ Configurator/project/requests.py
@@ -1,21 +1,23 @@
+import os
 import requests
+from dotenv import load_dotenv
 
-
+load_dotenv()
 def request_simulator(body):
 	return requests.post(
-		"http://localhost:5001/configure",
+		f"{os.getenv('SIMULATOR_HOST')}/configure",
 		json=body
 	)
 
 def request_observer(body):
 	return requests.post(
-		"http://localhost:5002/configure",
+		f"{os.getenv('OBSERVER_HOST')}/configure",
 		json=body
 	)
 
 def request_effector(body):
 	return requests.post(
-		"http://localhost:5003/configure",
+		f"{os.getenv('EFFECTOR_HOST')}/configure",
 		json=body
 	)
 
