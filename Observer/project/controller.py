fb6fb9cb993d71387ae4f37292a3da0fea5325b9
Adrilene <adrilenefonseca@gmail.com>
2023-10-01 16:16:01 -0300

    fix unused imports

--- Observer/project/controller.py
+++ Observer/project/controller.py
@@ -1,7 +1,7 @@
 from flask import jsonify, request
 from project import app
 
-from .observer import Observer, adaptation_status, cautious_status
+from .observer import Observer
 
 observer = None
 
@@ -17,16 +17,3 @@ def configure():
     observer.start()
 
     return jsonify({"msg": "Observer Start"})
-
-
-@app.route("/get_adaptation_status", methods=["GET"])
-def get_adaptation_status():
-    if request.args.get("type") == "adaptation":
-        if observer.adaptation_status or adaptation_status:
-            return jsonify(observer.adaptation_status), 200
-        return jsonify(observer.adaptation_status), 400
-
-    if request.args.get("type") == "cautious":
-        if observer.cautious_status or cautious_status:
-            return jsonify(observer.cautious_status), 200
-        return jsonify(observer.cautious_status), 400
